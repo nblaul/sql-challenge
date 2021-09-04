386eab9f978f0ab654eb3e8a65fbc90dd08623c5
Nicholas Blaul <nicholas.blaul@gmail.com>
2021-09-04 11:57:06 -0500

    updated sql query file

--- queries.sql
+++ queries.sql
@@ -59,4 +59,4 @@ WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
 SELECT last_name, COUNT(last_name) AS "Last Name Count"
 FROM employees
 GROUP BY last_name
-ORDER BY last_name DESC
+ORDER BY "Last Name Count" DESC
